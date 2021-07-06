6374c4a45a91de6b0d292939477ef3e4ca811e78
Yeliana Torres <yelis124@gmail.com>
2021-07-05 20:30:42 -0500

    fix: remove knobs

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
-    "@storybook/addon-knobs",
+    // "@storybook/addon-knobs",
   ],
   typescript: {
     reactDocgen: "none",
