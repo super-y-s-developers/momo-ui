9b9c6b549a687e907d8a9499cc3c1bb41a057069
Yeliana Torres <yelis124@gmail.com>
2021-07-05 20:56:54 -0500

    feat: create all button stories

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -20,9 +20,76 @@ export default {
 } as Meta;
 
 const Template: Story<ButtonProps> = (args) => <Button {...args}>Hello</Button>;
+const TemplateSecondary: Story<ButtonProps> = (args) => <SecondaryButton {...args}>Hello</SecondaryButton>;
+const TemplateTertiary: Story<ButtonProps> = (args) => <TertiaryButton {...args}>Hello</TertiaryButton>;
 
-export const Primary = Template.bind({});
+export const Default = Template.bind({});
 
-export const Secondary = () => <SecondaryButton>Hello</SecondaryButton>;
+export const Secondary = TemplateSecondary.bind({});
 
-export const Tertiary = () => <TertiaryButton>Hello</TertiaryButton>;
+export const Tertiary = TemplateTertiary.bind({});
+
+export const Large = Template.bind({});
+Large.args = {
+  modifiers: "large"
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  modifiers: "small"
+};
+
+export const Icon = Template.bind({});
+Icon.args = {
+  modifiers: "icon"
+};
+
+export const LabelledIcon = Template.bind({});
+LabelledIcon.args = {
+  modifiers: "labelled-icon"
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+  modifiers: "warning"
+};
+
+export const WarningSecondary = TemplateSecondary.bind({});
+WarningSecondary.args = {
+  modifiers: "warningSecondary"
+};
+
+export const WarningTertiary = TemplateTertiary.bind({});
+WarningTertiary.args = {
+  modifiers: "warningTertiary"
+};
+
+export const Error = Template.bind({});
+Error.args = {
+  modifiers: "error"
+};
+
+export const ErrorSecondary = TemplateSecondary.bind({});
+ErrorSecondary.args = {
+  modifiers: "errorSecondary"
+};
+
+export const ErrorTertiary = TemplateTertiary.bind({});
+ErrorTertiary.args = {
+  modifiers: "errorTertiary"
+};
+
+export const Success = Template.bind({});
+Success.args = {
+  modifiers: "success"
+};
+
+export const SuccessSecondary = TemplateSecondary.bind({});
+SuccessSecondary.args = {
+  modifiers: "successSecondary"
+};
+
+export const SuccessTertiary = TemplateTertiary.bind({});
+SuccessTertiary.args = {
+  modifiers: "successTertiary"
+};
