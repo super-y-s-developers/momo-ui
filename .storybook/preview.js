0cd7ea3d2aa3e4b6783cd03368e9dc724cc18295
kevinmalaverr <kevinmalaverr@gmail.com>
2021-07-20 10:12:20 -0500

    Add modal

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,14 @@
 import momoTheme from "./MomoTheme";
 import { addDecorator } from "@storybook/react";
 import { GlobalStyle } from "../src/utils/Global";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "../src/utils/themes";
 
 addDecorator((story) => (
-  <>
+  <ThemeProvider theme={defaultTheme}>
     {story()}
     <GlobalStyle />
-  </>
+  </ThemeProvider>
 ));
 
 export const parameters = {
