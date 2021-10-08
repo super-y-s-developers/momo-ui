c2fd966620e2a375eae88602c6f23d01e02086a3
Yeliana Torres <yelis124@gmail.com>
2021-10-08 01:14:32 -0500

    Button loading spinner (#29)
    * update
    
    * create story as we expect it to work
    
    * fix padding
    
    * add animation to loader
    
    * v0.0.9
    
    * fic: use button IntrinsicElements
    
    * v0.0.10
    
    * v0.0.11
    
    * exclude unnecessary files for build
    
    * add Props for secondary tertiary buttos to work
    
    * v0.0.12
    
    * fix ts bug

--- src/components/Button.stories.tsx
+++ src/components/Button.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Story, Meta } from '@storybook/react';
+import { Story, Meta } from "@storybook/react";
 
 import Button, {
   SecondaryButton,
@@ -16,14 +16,29 @@ export default {
       options: Object.keys(BUTTON_MODIFIERS),
       control: { type: "check" },
     },
+    loading: {
+      control: { type: "boolean" },
+    },
   },
 } as Meta;
 
+// @ts-ignore
 const Template: Story<ButtonProps> = (args) => <Button {...args}>Hello</Button>;
-const TemplateSecondary: Story<ButtonProps> = (args) => <SecondaryButton {...args}>Hello</SecondaryButton>;
-const TemplateTertiary: Story<ButtonProps> = (args) => <TertiaryButton {...args}>Hello</TertiaryButton>;
+const TemplateSecondary: Story<ButtonProps> = (args) => (
+  // @ts-ignore
+  <SecondaryButton {...args}>Hello</SecondaryButton>
+);
+const TemplateTertiary: Story<ButtonProps> = (args) => (
+  // @ts-ignore
+  <TertiaryButton {...args}>Hello</TertiaryButton>
+);
 
 export const Default = Template.bind({});
+
+export const Loading = Template.bind({});
+Loading.args = {
+  loading: true,
+};
 
 export const Secondary = TemplateSecondary.bind({});
 
@@ -31,65 +46,65 @@ export const Tertiary = TemplateTertiary.bind({});
 
 export const Large = Template.bind({});
 Large.args = {
-  modifiers: "large"
+  modifiers: "large",
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  modifiers: "small"
+  modifiers: "small",
 };
 
 export const Icon = Template.bind({});
 Icon.args = {
-  modifiers: "icon"
+  modifiers: "icon",
 };
 
 export const LabelledIcon = Template.bind({});
 LabelledIcon.args = {
-  modifiers: "labelled-icon"
+  modifiers: "labelled-icon",
 };
 
 export const Warning = Template.bind({});
 Warning.args = {
-  modifiers: "warning"
+  modifiers: "warning",
 };
 
 export const WarningSecondary = TemplateSecondary.bind({});
 WarningSecondary.args = {
-  modifiers: "warningSecondary"
+  modifiers: "warningSecondary",
 };
 
 export const WarningTertiary = TemplateTertiary.bind({});
 WarningTertiary.args = {
-  modifiers: "warningTertiary"
+  modifiers: "warningTertiary",
 };
 
 export const Error = Template.bind({});
 Error.args = {
-  modifiers: "error"
+  modifiers: "error",
 };
 
 export const ErrorSecondary = TemplateSecondary.bind({});
 ErrorSecondary.args = {
-  modifiers: "errorSecondary"
+  modifiers: "errorSecondary",
 };
 
 export const ErrorTertiary = TemplateTertiary.bind({});
 ErrorTertiary.args = {
-  modifiers: "errorTertiary"
+  modifiers: "errorTertiary",
 };
 
 export const Success = Template.bind({});
 Success.args = {
-  modifiers: "success"
+  modifiers: "success",
 };
 
 export const SuccessSecondary = TemplateSecondary.bind({});
 SuccessSecondary.args = {
-  modifiers: "successSecondary"
+  modifiers: "successSecondary",
 };
 
 export const SuccessTertiary = TemplateTertiary.bind({});
 SuccessTertiary.args = {
-  modifiers: "successTertiary"
+  modifiers: "successTertiary",
 };
