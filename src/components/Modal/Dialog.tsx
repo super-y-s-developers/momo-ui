a4733f333ac914fd68764878aeb9f7c764d098bb
Kevin Malaver <kevinmalaverr@gmail.com>
2021-10-16 12:56:46 -0500

    Fix styled import

--- src/components/Modal/Dialog.tsx
+++ src/components/Modal/Dialog.tsx
@@ -1,4 +1,4 @@
-import { styled } from "styled-components";
+import styled from "styled-components";
 import React from "react";
 import { Button } from "..";
 import { TertiaryButton } from "../Button";
